f70f45cd378ae7d338f4c27e626f18c83a98409d
Hans Leonel <hansleonel@gmail.com>
2019-11-18 18:14:19 -0500

    sidebar

--- src/main/webapp/app/layouts/admin-layout/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/admin-layout/sidebar/sidebar.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  valoresRutas: object;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.valoresRutas = [
+      {
+        link: '',
+        icon: 'database',
+        text: 'bienes',
+        children: [
+          {
+            link: '/bienes-muebles',
+            icon: 'database',
+            text: 'bienes muebles'
+            // children: []
+          },
+          {
+            link: '/asignaciones-muebles',
+            icon: 'database',
+            text: 'asiganciones muebles'
+            // children: []
+          }
+        ]
+      }
+    ];
+  }
 }
